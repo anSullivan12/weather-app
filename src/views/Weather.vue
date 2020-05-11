0de4f57f78299fa1b4592db15f9e9c3df01e4cc1
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-11 14:13:34 -0400

    rearrange and add content

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="weather mui-container-fluid">
-    <h1 class="mui--text-display3">Local Weather</h1>
+  <div class="mui-container weather">
+    <h1 class="mui--text-display1">Local Weather</h1>
     <form class="mui-form">
       <div class="mui-textfield mui-textfield--float-label">
         <input type="text">
@@ -8,10 +8,16 @@
       </div>
       <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
     </form>
-    <div class="mui-container-fluid weather-results">
-      <span class="material-icons">wb_sunny</span>
-      <h2 class="mui--text-display2 temp">72&deg;</h2>
-      <p class="mui--text-body1 forecast">Sunny with a high of 75</p>
+    <div class="mui-row mui-panel weather-results">
+      <div class="mui-col-md-8 mui--text-left">
+        <p class="mui-subhead">North Canton, OH</p>
+        <p class="mui-subhead">Date</p>
+        <span class="material-icons">wb_sunny</span>
+        <p class="mui--text-body1 forecast">Sunny with a high of 75</p>
+      </div>
+      <div class="mui-col-md-4">
+        <h2 class="mui--text-display4 temp">72&deg;</h2>
+      </div>
     </div>
   </div>
 </template>
