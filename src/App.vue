ae153f9a22cec74bc5765729365250e68f225c88
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-15 11:18:46 -0400

    use names instead of direct links for the nav links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/weather">Weather</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'Weather' }">Weather</router-link>
     </div>
     <router-view/>
   </div>
