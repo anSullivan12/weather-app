e61997250be88b61513204c355d2fbdf9437099f
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-21 23:09:48 -0400

    comment out One Day component and get the data on to the actual page

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="mui-container weather">
     <h1 class="mui--text-display1">Local Weather for 44720</h1>
-    <OneDay v-for="detail in details" :key="detail.count" :detail="detail" />
+    {{ details }}
+    <!-- <OneDay v-for="(detail, index) in details" :key="index" :detail="detail" /> -->
   </div>
 </template>
 
 <script>
-import OneDay from '@/components/OneDay.vue';
+// import OneDay from '@/components/OneDay.vue';
 import WeatherService from '@/services/WeatherService';
 
 export default {
   name: 'Weather',
-  components: {
-    OneDay,
-  },
+  // components: {
+  //   OneDay,
+  // },
   data() {
     return {
       details: [],
