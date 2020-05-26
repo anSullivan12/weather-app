3b1a2d33e8003a1595f0318271336f7075448f41
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-26 10:49:01 -0400

    capture zip code input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mui-container weather">
     <h1 class="mui--text-display1">Local Weather</h1>
-    <form class="mui-form">
+    <form class="mui-form" @submit.prevent="submitZip">
       <div class="mui-textfield mui-textfield--float-label">
-        <input type="text">
+        <input type="text" name="zip" v-model="zip">
         <label>Zip Code</label>
       </div>
       <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
@@ -14,5 +14,15 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      zip: null,
+    };
+  },
+  methods: {
+    submitZip() {
+      console.log(this.zip);
+    },
+  },
 };
 </script>
