442eb043c841238c779190cb917c59bc319ef0eb
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-26 16:30:15 -0400

    create component for zip code submission

---
+++ src/components/ZipSubmit.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="mui-form" @submit.prevent="submitZip">
+    <div class="mui-textfield mui-textfield--float-label">
+      <input type="text" name="zip" v-model="zip">
+      <label>Zip Code</label>
+    </div>
+    <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      zip: null,
+    };
+  },
+  methods: {
+    submitZip() {
+      console.log(this.zip);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,17 @@
 <template>
   <div class="mui-container weather">
     <h1 class="mui--text-display1">Local Weather</h1>
-    <form class="mui-form" @submit.prevent="submitZip">
-      <div class="mui-textfield mui-textfield--float-label">
-        <input type="text" name="zip" v-model="zip">
-        <label>Zip Code</label>
-      </div>
-      <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
-    </form>
+    <ZipSubmit/>
   </div>
 </template>
 
 <script>
+import ZipSubmit from '@/components/ZipSubmit.vue';
+
 export default {
   name: 'Home',
-  data() {
-    return {
-      zip: null,
-    };
-  },
-  methods: {
-    submitZip() {
-      console.log(this.zip);
-    },
+  components: {
+    ZipSubmit,
   },
 };
 </script>
