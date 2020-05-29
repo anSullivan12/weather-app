c716729d94bec019225fadee256e35e66f44dba8
Ashley Sullivan <ansullivan12@gmail.com>
2020-05-29 12:54:25 -0400

    add units to api call to get Farenheit

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://api.weatherbit.io/v2.0/current?country=US&key=25201b6605424961868883102857e8b9&postal_code=',
+  baseURL: 'https://api.weatherbit.io/v2.0/current?units=I&country=US&key=25201b6605424961868883102857e8b9&postal_code=',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
