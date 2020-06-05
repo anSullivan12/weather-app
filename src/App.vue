49e0d0ee035655e65f62f7a35b063a188bc20a43
Ashley Sullivan <ansullivan12@gmail.com>
2020-06-05 15:50:32 -0400

    add styling for light and dark, move button and its script to component

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,65 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'Home' }">Home</router-link> |
-      <router-link :to="{ name: 'UserPreferences' }">Preferences</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
     </div>
     <router-view/>
-    <div>
-      <button class="mui-btn mui-btn--raised mui-btn--danger" :title="theme == 'dark' ? 'Dark Theme' : 'Light Theme'" @click="toggleTheme()">Theme</button>
-    </div>
+    <UserPreferences/>
   </div>
 </template>
 
 <script>
+import UserPreferences from '@/components/UserPreferences.vue';
+
 export default {
-  data() {
-    return {
-      theme: localStorage.getItem('theme'),
-    };
-  },
-  created() {
-    if (this.theme === 'light') {
-      console.log('user has light theme selected');
-    } else {
-      document.body.setAttribute('class', 'darkBody');
-    }
-  },
-  methods: {
-    toggleTheme() {
-      if (this.theme === 'dark') {
-        this.theme = 'light';
-        document.body.setAttribute('class', 'light');
-        localStorage.setItem('theme', 'light');
-      } else {
-        this.theme = 'dark';
-        document.body.setAttribute('class', 'darkBody');
-        localStorage.setItem('theme', 'dark');
-      }
-    },
+  components: {
+    UserPreferences,
   },
 };
 </script>
 
 <style lang="scss">
-.darkBody {
-  background-color: #2c3e50;
-  h1,h2,h3,h4,h5,h6,p {
-    color: #42b983;
-  }
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  }
+}
+.mui-panel {
+  margin-right: 20px;
+}
+body.darkBody {
+  background-color: #212121;
+  color: #FAFAFA;
+  #app,
+  .mui-textfield>input:focus~label, .mui-textfield>textarea:focus~label {
+    color: #FAFAFA;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    color: #FFB74D;
+  }
+  #nav a {
+    color: #E0F7FA;
+  }
+  .mui-textfield>input {
+    border-bottom: 1px solid rgba(255,255,255,.26);
+    color: #FAFAFA;
+  }
+  .mui-panel {
+    background-color: #424242;
+    box-shadow: 0 2px 2px 0 rgba(255,255,255,.16), 0 0 2px 0 rgba(255,255,255,.12);
+    color: #FAFAFA;
+  }
+  .mui-btn:focus,
+  .mui-btn:hover {
+    box-shadow: 0 0 2px rgba(255,255,255,.12), 0 2px 2px rgba(255,255,255,.2);
   }
 }
 </style>
