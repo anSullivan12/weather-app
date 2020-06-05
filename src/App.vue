105239bc340677db1b3028d4f955460b6e7550e7
Ashley Sullivan <ansullivan12@gmail.com>
2020-06-05 12:39:00 -0400

    get theme switch working via button

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,43 @@
       <router-link :to="{ name: 'UserPreferences' }">Preferences</router-link>
     </div>
     <router-view/>
+    <div>
+      <button class="mui-btn mui-btn--raised mui-btn--accent" :title="theme == 'dark' ? 'Dark Theme' : 'Light Theme'" @click="toggleTheme()">Theme</button>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      theme: localStorage.getItem('theme'),
+    };
+  },
+  methods: {
+    toggleTheme() {
+      if (this.theme === 'dark') {
+        this.theme = 'light';
+        document.body.setAttribute('class', 'light');
+        localStorage.setItem('theme', 'light');
+      } else {
+        this.theme = 'dark';
+        document.body.setAttribute('class', 'darkBody');
+        localStorage.setItem('theme', 'dark');
+      }
+    },
+  },
+};
+</script>
+
 <style lang="scss">
+.darkBody {
+  background-color: #2c3e50;
+  h1,h2,h3,h4,h5,h6,p {
+    color: #42b983;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
