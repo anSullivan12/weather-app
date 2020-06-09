e4e3c86034de5c45794f1abc375410abf2937ffb
Ashley Sullivan <ansullivan12@gmail.com>
2020-06-09 10:18:31 -0400

    no longer have feels like, display high and low temp

--- src/components/MultiDay.vue
+++ src/components/MultiDay.vue
@@ -3,7 +3,8 @@
     <p class="mui--text-caption">{{ forecast.datetime }}</p>
     <p class="mui--text-caption">{{ forecast.pod }}</p>
     <h2 class="mui--text-display3">{{ forecast.temp }}&deg;</h2>
-    <p class="mui--text-body2">Feels like {{ forecast.app_temp }}&deg;</p>
+    <p class="mui--text-body2">High {{ forecast.high_temp }}&deg;</p>
+    <p class="mui--text-body2">Low {{ forecast.low_temp }}&deg;</p>
     <img :src="require('@/assets/icons/' + forecast.weather.icon + '.png')">
     <p class="mui--text-body1">{{ forecast.weather.description }}</p>
     <p class="mui--text-body1">Chance of Rain {{ forecast.pop }}%</p>
